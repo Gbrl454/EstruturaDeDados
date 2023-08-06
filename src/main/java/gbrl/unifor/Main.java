8a65a5d8809e74ed627583c610f37610dcd93c1f
Gabriel Matos <gabriel.matos@edu.unifor.br>
2023-08-06 19:56:33 -0300

    Questão 6

---
+++ src/main/java/gbrl/unifor/Main.java
@@ -0,0 +1,31 @@
+package gbrl.unifor;
+
+import gbrl.unifor.exercicios.*;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static void valorRes(Scanner sc) {
+        System.out.println("Selecione uma questão...");
+        int res = sc.nextInt();
+        escolha(sc, res);
+    }
+
+    private static void escolha(Scanner sc, int res) {
+        switch (res) {
+            case 1 -> Exercicio1.run(sc);
+            case 2 -> Exercicio2.run(sc);
+            case 3 -> Exercicio3.run(sc);
+            case 4 -> Exercicio4.run(sc);
+            case 5 -> Exercicio5.run(sc);
+            case 6 -> Exercicio6.run(sc);
+            default -> valorRes(sc);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        valorRes(sc);
+    }
+}

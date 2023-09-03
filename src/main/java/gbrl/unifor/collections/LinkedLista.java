db77baad6b32454df69ff7a33e3c07c11b489479
Gabriel Matos <gabriel.matos@edu.unifor.br>
2023-09-03 20:28:45 -0300

    Criando LinkedLista

---
+++ src/main/java/gbrl/unifor/collections/LinkedLista.java
@@ -0,0 +1,172 @@
+package gbrl.unifor.collections;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class LinkedLista<E> implements Lista<E> {
+    private Node primeiro;
+    private Node ultimo;
+
+    // TODO
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    // TODO 
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    // TODO 
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    // TODO 
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    // TODO 
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    // TODO 
+    @Override
+    public void clear() {
+
+    }
+
+    // TODO 
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    // TODO 
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    // TODO 
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    // TODO 
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    // TODO 
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    // TODO 
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    // TODO 
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    // TODO 
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    // TODO 
+    @Override
+    public Lista<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        Node atual = primeiro;
+        String str = "[";
+        while (atual != null) {
+            str += atual.item + ", ";
+            atual = atual.proximo;
+        }
+        return str;
+    }
+
+    private static class Node<E> {
+        E item;
+        Node<E> anterior;
+        Node<E> proximo;
+
+        Node(Node<E> proximo, E item, Node<E> anterior) {
+            this.item = item;
+            this.anterior = anterior;
+            this.proximo = proximo;
+        }
+    }
+}
+
--- src/main/java/gbrl/unifor/collections/Teste.java
+++ src/main/java/gbrl/unifor/collections/Teste.java
@@ -1,42 +1,19 @@
 package gbrl.unifor.collections;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Teste {
     public static void main(String[] args) {
-        ArrayList<Double> doubleListADD = new ArrayList<>();
-        doubleListADD.add(1.1);
-        doubleListADD.add(2.2);
-        doubleListADD.add(3.3);
-        doubleListADD.add(4.4);
-        doubleListADD.add(5.5);
-        ArrayList doubleListREM = new ArrayList<>(doubleListADD);
-        doubleListREM.add("A");
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("A");
+        linkedList.add("B");
+        linkedList.add("C");
+        System.out.println(linkedList);
 
-        ArrayLista<Double> lista = new ArrayLista<>();
-        System.out.println("ADD");
-        lista.add(1.0);
-        lista.add(3.0);
-        lista.add(5.0);
-        System.out.println(lista);
-        System.out.println("ADDALL");
-        lista.addAll(doubleListADD);
-        System.out.println(lista);
-        System.out.println("REMOVEALL");
-        lista.removeAll(doubleListREM);
-        System.out.println(lista);
-
-        ArrayList<Double> list = new ArrayList<>();
-        System.out.println("ADD");
-        list.add(1.0);
-        list.add(3.0);
-        list.add(5.0);
-        System.out.println(list);
-        System.out.println("ADDALL");
-        list.addAll(doubleListADD);
-        System.out.println(list);
-        System.out.println("REMOVEALL");
-        list.removeAll(doubleListREM);
-        System.out.println(list);
+        LinkedLista<String> linkedLista = new LinkedLista<>();
+        linkedLista.add("A");
+        linkedLista.add("B");
+        linkedLista.add("C");
+        System.out.println(linkedLista);
     }
 }
